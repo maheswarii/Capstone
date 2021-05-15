da31fe70b591151c3cd8b970fe87b341d06d2deb
maheswarii <62419626+maheswarii@users.noreply.github.com>
2021-05-15 22:32:28 +0700

    update2

---
+++ app/src/main/java/com/maheswari/capstonebismillah/start/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.maheswari.capstonebismillah.start
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.maheswari.capstonebismillah.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/maheswari/capstonebismillah/start/StartActivity.kt
@@ -0,0 +1,12 @@
+package com.maheswari.capstonebismillah.start
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.maheswari.capstonebismillah.R
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
