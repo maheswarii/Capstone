e482f1824331e76ebf1b349308c7d2fe03881050
maheswarii <62419626+maheswarii@users.noreply.github.com>
2021-05-21 21:25:44 +0700

    update progres login

--- app/src/main/java/com/yps/layani/admin/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/yps/layani/admin/ui/profile/ProfileFragment.kt
@@ -8,7 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.maheswari.capstonebismillah.R
+import com.yps.layani.R
 
 class ProfileFragment : Fragment() {
 
