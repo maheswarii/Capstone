8f23c3ea45141138688010f5a94b6c6a06e3e21a
cabel <cantikabelia01@gmail.com>
2021-05-27 02:00:11 +0700

    rv

--- app/src/main/java/com/yps/layani/admin/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/yps/layani/admin/ui/profile/ProfileFragment.kt
@@ -1,17 +1,27 @@
 package com.yps.layani.admin.ui.profile
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.yps.layani.admin.api.ApiService
+import com.yps.layani.admin.response.UserResponse
 import com.yps.layani.databinding.FragmentProfileBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
 
+    var userId: String = "";
 
+    private lateinit var txt_name: TextView
+    private lateinit var txt_email: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -19,5 +29,20 @@ class ProfileFragment : Fragment() {
 
         _binding = FragmentProfileBinding.inflate(inflater, container, true)
         return binding.root
+
+//        getUser()
     }
+//    private fun getUser() {
+//        ApiService.loginApiCall().getUser(userId).enqueue(object : Callback<UserResponse> {
+//            override fun onResponse(
+//                call: Call<UserResponse>,
+//                response: Response<UserResponse>
+//            ) {
+//                Log.d("Response User ::::", response.body().toString())
+//                if (response.body()!!.status) {
+//                    txt_name.setText(response.body()!!.data.username)
+//                    txt_email.setText(response.body()!!.data.email)
+//                }
+//            }
+
 }
